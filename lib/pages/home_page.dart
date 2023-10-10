7af754f75942792a155670daa3aa118cbc832749
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-10 13:47:27 -0300

    add columns and rows

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,10 +12,31 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Card(
-            child: Padding(
+            child: Container(
+              width: double.infinity,
               padding: const EdgeInsets.all(8.0),
               child: Image.asset('images/rocket.png'),
             ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Card(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('images/space.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Card(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('images/rocket.png'),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
