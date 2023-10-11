0a4206f15a372d3174f4de03d751f1eb83872e67
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-11 15:18:24 -0300

    add button

--- lib/pages/description_page.dart
+++ lib/pages/description_page.dart
@@ -2,26 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter_youtube/class/item_class.dart';
 import 'package:flutter_youtube/core/constants.dart';
 
-class DescriptionPage extends StatelessWidget {
+class DescriptionPage extends StatefulWidget {
   const DescriptionPage({super.key, required this.item});
   final ItemClass item;
+
+  @override
+  State<DescriptionPage> createState() => _DescriptionPageState();
+}
+
+class _DescriptionPageState extends State<DescriptionPage> {
+  double titleFontSize = 25;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(item.title),
+        title: Text(widget.item.title),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(kDouble10),
           child: Column(
             children: [
-              Image.asset(item.imagePath),
+              Image.asset(widget.item.imagePath),
+              Wrap(
+                spacing: kDouble10,
+                children: [
+                  TextButton(
+                    onPressed: () => setState(() => titleFontSize = 25),
+                    child: const Text('Small Title'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () => setState(() => titleFontSize = 50),
+                    child: const Text('Medium Title'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => setState(() => titleFontSize = 100),
+                    child: const Text('Large Title'),
+                  ),
+                  FilledButton(
+                    onPressed: () => setState(() => titleFontSize = 200),
+                    child: const Text('HugeTitle'),
+                  ),
+                ],
+              ),
               FittedBox(
                 child: Text(
-                  item.title,
-                  style: const TextStyle(
-                      fontSize: 200, fontWeight: FontWeight.bold),
+                  widget.item.title,
+                  style: TextStyle(
+                    fontSize: titleFontSize,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               const Text(
